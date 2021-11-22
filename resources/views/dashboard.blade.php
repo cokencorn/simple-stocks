581d8d931aa65aaef653df7e3c673f67447ab0d9
Altay Adademir <altayadademir@gmail.com>
2021-11-23 00:48:41 +0200

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,16 +5,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
+                    <div class="card-header">Güvenli Anasayfa</div>
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        Giriş yaptınız. Menüyü kullanarak yapılacak işlemi seçebilirsiniz.
+                        Menüyü kullanarak yapılacak işlemi seçebilirsiniz.
                     </div>
                 </div>
             </div>
