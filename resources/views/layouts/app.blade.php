788dc92a9554ef2c8a3c3df8507573f54cf71fee
Altay Adademir <altayadademir@gmail.com>
2021-11-23 00:38:20 +0200

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,12 +57,6 @@
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                         @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
                     @else
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('stock') }}">Malzeme Ekle</a>
